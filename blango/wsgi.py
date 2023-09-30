bf6498fb90880408ffbf7d4d034105377046f70b
MUSTAFA ALHAIBA <moustafa.alhaiba.2003@gmail.com>
2023-09-30 18:39:27 +0000

    Finish django configurations

--- blango/wsgi.py
+++ blango/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blango.settings')
-
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
