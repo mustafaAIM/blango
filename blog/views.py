3eb835c79bc33af4e58ddfde75b95d2f482c2677
MUSTAFA ALHAIBA <moustafa.alhaiba.2003@gmail.com>
2023-10-01 11:37:04 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,15 @@ from blog.models import Post
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 from blog.forms import CommentForm
+import logging
 # Create your views here.
+logger = logging.getLogger(__name__)
+
+
+
 def index(request):
     posts = Post.objects.all()
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 
@@ -20,6 +26,7 @@ def post_detail(request, slug):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                logger.info("Created comment on Post %d for user %s", post.pk, request.user)
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
