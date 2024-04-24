67ae1c424b9bb773f932d9004cd6158b093d67ca
MUSTAFA ALHAIBA <moustafa.alhaiba.2003@gmail.com>
2024-04-24 21:16:50 +0000

    Finish simplejwt

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -6,7 +6,7 @@ from drf_yasg  import openapi
 from drf_yasg.views import get_schema_view 
 import os 
 from rest_framework.routers import DefaultRouter 
-
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 router = DefaultRouter()
 router.register("tags",TagViewSet)
 router.register("posts", PostViewSet)
@@ -43,4 +43,6 @@ urlpatterns = [
         PostViewSet.as_view({"get": "list"}),
         name="posts-by-time",
     ),
+        path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
 ]
