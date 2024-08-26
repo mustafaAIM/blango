2a315d9d74509b013ecb8102367b5a91a45afb08
MUSTAFA ALHAIBA <moustafa.alhaiba.2003@gmail.com>
2024-08-26 15:42:01 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,24 @@
-const theNumber = 1
-let yourName = 'Ben'
+class DelayedGreeter extends Greeter {
+  delay = 2000
 
-if (theNumber === 1) {
-  let yourName = 'Leo'
-  alert(yourName)
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
 }
 
-alert(yourName)
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
+
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
